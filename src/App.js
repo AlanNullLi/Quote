bfa4a48aa6f15365ed65e24a86453d0b3eb80eee
Alan Li <al2hkm@virginia.edu>
2020-04-19 14:02:34 -0400

    Quote commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: null
+    }
+  }
+
+  getQuote = () => {
+    axios
+      .get("http://quotes.stormconsultancy.co.uk/random.json")
+      .then(response => {
+        this.setState({
+          quote: response.data
+        })
+        console.log(response.data.quote);
+      })
+  }
+
+  changeQuote = (str) => {
+    this.setState(prevState => {
+      return { quote: str };
+    });
+  };
+
+  render() {
+    let q = "Click the button for a quote!"
+    let start = <button onClick={() => this.getQuote()}>Get Quote!</button>
+    if (this.state.quote) {
+      start = <button onClick={() => this.getQuote()}>Get New Quote!</button>
+      q = this.state.quote.quote
+    }
+
+    return (
+      <div>
+
+        <div>
+          {start}
+        </div>
+        <div className="quote">
+          <h3>{q}</h3>
+        </div>
+
+      </div>
+    )
+
+  }
+
+
 }
 
 export default App;
